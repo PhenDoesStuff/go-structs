04ce2724c361858407813ba412b0241b02f0e485
Stephen Montague <stephen.d.montague@gmail.com>
2021-11-05 09:38:00 -0400

    Learning to add a nested struct

--- main.go
+++ main.go
@@ -2,16 +2,40 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zioCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	var stephen person
+	// Way of creating a struct
+	// stephen := person{firstName: "Stephen", lastName: "Montague"}
 
+	// Another way of creating a struct:
+	// var stephen person
 
+	// stephen.firstName = "Stephen"
+	// stephen.lastName = "Montague"
 
-	fmt.Println(stephen)
-	fmt.Printf("%+v", stephen)
+	// fmt.Println(stephen)
+
+	// --------------------------------------
+
+	// New Person with a Contact:
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Halpert",
+		contact: contactInfo{
+			email:   "jim.halpert@dundermiflin.com",
+			zioCode: 18501,
+		},
+	}
+
+	fmt.Printf("%+v", jim)
 }
